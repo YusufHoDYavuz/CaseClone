2f669a934b4c74492bbd0a8e58fe922c54a2706e
Yusuf <veloncys@gmail.com>
2024-06-20 16:23:55 +0300

    Add: Camera Movement

---
+++ Assets/Game/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace XGames.GameName
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [Header("Movement")]
+        [SerializeField] private Transform target;
+        [SerializeField] private float smoothSpeed;
+        [SerializeField] private Vector3 offset;
+
+        [Header("Limit")]
+        [SerializeField] private float limit;
+
+        private void LateUpdate()
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            smoothedPosition.x = Mathf.Clamp(smoothedPosition.x, -limit, limit);
+            transform.position = smoothedPosition;
+        }
+    }
+}
